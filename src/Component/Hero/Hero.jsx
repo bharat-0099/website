2273e9d97d06b8692ed7bbc1ef31b3d178d0cf3f
Bharat Chundru <chundrubharat6699@gmail.com>
2024-11-07 12:39:17 -0500

    Modified Changes inHero.JSX

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -12,21 +12,29 @@ const Hero = () => {
       <img src={profilepicture} alt="profile" className='pics' />
       <h1>I'm Bharat Chundru,</h1>
       <p>
-        Computer Science Graduate from University of Illinois Springfield, I hold both a Bachelor's and Master's degree in Computer Science. I have a strong understanding of algorithms, data structures, and software engineering principles through my academic career. I took an active role in projects that strengthened my knowledge of programming languages including Java, Python, and C++ during my education. I refined my problem-solving skills and created a methodical approach to software development through coursework and group projects.
+        Computer Science professional holding both a Bachelor's and Master's degree from the University of Illinois Springfield. My academic path has equipped me with a deep understanding of algorithms, data structures, and software engineering practices, all essential building blocks for creating efficient, high-quality software solutions.
+
+
+      </p>
+      <p>
+      During my studies, I immersed myself in projects that expanded my proficiency in programming languages like Java, Python, and C++. These experiences didn’t just enhance my technical knowledge; they refined my problem-solving skills and instilled in me a structured, methodical approach to development. Whether working solo or within a team, I thrive on tackling complex challenges and bringing ideas to life through code.
+      </p>
+      <p>
+      Now, as an aspiring software Engineer, I’m excited to channel this foundation into impactful, user-centered solutions. Explore my portfolio to discover my skills, projects, and vision as I work towards making a meaningful contribution to the tech world!
       </p>
 
       <div className='hero-action'>
         <a href={resume} download className="hero-resume">
           <img src={res} alt="Resume" />
         </a>
-        
-        <a href="https://www.linkedin.com/in/bharat-chundru" className='hero-resume'>
-          <img  src={linkedin} alt="LinkedIn" /></a>
 
-          <a href="https://github.com/bharat-0099" className='hero-resume'>
-          <img  src={hub} alt="Git hub" /></a>
-          
-        
+        <a href="https://www.linkedin.com/in/bharat-chundru" className='hero-resume'>
+          <img src={linkedin} alt="LinkedIn" /></a>
+
+        <a href="https://github.com/bharat-0099" className='hero-resume'>
+          <img src={hub} alt="Git hub" /></a>
+
+
       </div>
     </div>
   );
